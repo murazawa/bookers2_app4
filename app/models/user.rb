31bee7dadf1c21d3f8b35ea653fd568bc9ae62e5
murazawa <ahahanoha.m0115@gmail.com>
2021-08-04 03:10:33 +0000

    [Update]

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,41 @@ class User < ApplicationRecord
 	has_many :favorites, dependent: :destroy
 	has_many :book_comments, dependent: :destroy
   attachment :profile_image, destroy: false
+  
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
 
+  
+  
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
+  
+  
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
 end
+
+# foregin_key = 入口
+# source = 出口
+
+# ・最大文字数：length: { maximum: 20 }
+# ・最小文字数：length: { minimum: 20 }
+# ・文字数の範囲：length: { in: 1..30 }
+# ・空でない：presence: true
+# ・他と被っていない：uniqueness: true
+# ・正規表現：format: { with: /<正規表現>/}
+# ・「<属性名>_confirmation」と一致：confirmation: true
